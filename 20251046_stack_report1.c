4af96e6c374a1f894acb01d7ba66be2adaf8662f
진수안 <20251046@vision.hoseo.edu>
2026-04-07 11:19:12 +0900

    Update 20251046_stack_report1.c

--- 20251046_stack_report1.c
+++ 20251046_stack_report1.c
@@ -9,18 +9,15 @@ Welcome to GDB Online.
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 100 //스택 최대 크기 정의
+#define MAX 100 
 
 int main()
 {
-    //1. 스택으로 사용할 문자 배열을 동적으로 할당
     char *stack = (char *)malloc(MAX * sizeof(char));
     int top = -1;
-    
-    //입력을 받을 배열 (최대 100자 + 개해문자 + 널문자 여유 공간)
+
     char input[MAX + 2];
-    
-    //메모리 할당 실패 시 안전하게 종료
+
     if(stack == NULL){
         printf("메모리 할당에 실패했습니다.\n");
         return -1;
@@ -28,24 +25,17 @@ int main()
     
     printf("문자열을 입력하세요 : ");
     if(fgets(input, sizeof(input), stdin) != NULL){
-        
-        //2.입력받은 문자열을 스택에 Push (LIFO 구조)
+     
         for(int i = 0;input[i] != '\0' && i < MAX;i++){
-            //엔터(줄바꿈) 문자는 무시하고 넘어감
             if(input[i] == '\n') continue;
-            
-            // stack 배열에 값을 넣고 top 위치 갱신 (Push 로직)
             stack[++top] = input[i];
         }
-        // 3. 스택에서 Pop하여 거꾸로 출력
         printf("거꾸로 출력된 문자열: ");
         while (top >= 0) {
-            // 맨 위(top)부터 차례대로 꺼내서 출력하고 top 감소 (Pop 로직)
             printf("%c", stack[top--]); 
         }
         printf("\n");
     }
-    // 4. 과제 조건: 프로그램 종료 시 메모리 해제
     free(stack);
     return 0;
 }
